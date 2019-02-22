d8d715ea537902c2bcd67adbf8a032e48e4f2803
Trond Aasan <trond.aasan@gmail.com>
2019-02-22 17:26:21 +0100

    Slå på linjenummer

--- personal/init.el
+++ personal/init.el
@@ -4,3 +4,7 @@
               (use-local-map (copy-keymap (current-local-map))))
             (when server-buffer-clients
               (local-set-key (kbd "C-x k") 'server-edit))))
+
+;; Slå på linjenummer
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
