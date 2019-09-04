d8516e8cf2bd5e11fd0877ce8b341bbbe1e46b73
Trond Aasan <trond.aasan@gmail.com>
2019-09-05 00:20:07 +0200

    Slå på company (completions)

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -6,7 +6,7 @@
 (require 'prelude-ivy) ;; A mighty modern alternative to ido
 ;; (require 'prelude-helm) ;; Interface for narrowing and search
 ;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
-;; (require 'prelude-company)
+(require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-mediawiki)
 ;; (require 'prelude-evil)
